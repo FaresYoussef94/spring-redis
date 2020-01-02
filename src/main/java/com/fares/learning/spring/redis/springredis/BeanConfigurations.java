c047cc20f940be274595ed8fcc3eb09240deca51
Fares Youssef <fares.youssef@seal-software.com>
2020-01-02 12:57:48 +0200

    using the default configuration to connect to redis

--- src/main/java/com/fares/learning/spring/redis/springredis/BeanConfigurations.java
+++ src/main/java/com/fares/learning/spring/redis/springredis/BeanConfigurations.java
@@ -24,12 +24,4 @@ public class BeanConfigurations {
 		return template;
 	}
 
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-		jedisConFactory.setHostName("localhost");
-		jedisConFactory.setPort(6379);
-		return jedisConFactory;
-	}
-
 }
