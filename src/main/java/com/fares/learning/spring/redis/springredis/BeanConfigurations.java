49d7d2962aa2f864ba393498451ff9805165f8a4
Fares Youssef <fares.youssef@seal-software.com>
2020-01-02 12:56:41 +0200

    adding boilerplate configuration

---
+++ src/main/java/com/fares/learning/spring/redis/springredis/BeanConfigurations.java
@@ -0,0 +1,35 @@
+package com.fares.learning.spring.redis.springredis;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.fares.learning.spring.redis" })
+public class BeanConfigurations {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+		jedisConFactory.setHostName("localhost");
+		jedisConFactory.setPort(6379);
+		return jedisConFactory;
+	}
+
+}
